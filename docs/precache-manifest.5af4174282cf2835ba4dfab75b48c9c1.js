fd984ec5157360861fa85e95c0624c7140f68101
Mauricio Melo <logonztech@gmail.com>
2022-01-12 13:05:10 -0600

    Agregando docs

---
+++ docs/precache-manifest.5af4174282cf2835ba4dfab75b48c9c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64e859a4451c7c4ccce9d81675d49a20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "388a34c3ea178d278f71",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388a34c3ea178d278f71",
+    "url": "/static/js/main.e0e03b38.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
